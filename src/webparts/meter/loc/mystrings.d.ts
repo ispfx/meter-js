58c2f8aeef2fdbf009087b019c5b4e8eed4b3810
kyleschaeffer <kyle@spirito.us>
2018-10-15 10:50:27 -0400

    Install SharePoint Framework solution

---
+++ src/webparts/meter/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface IMeterWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'MeterWebPartStrings' {
+  const strings: IMeterWebPartStrings;
+  export = strings;
+}
